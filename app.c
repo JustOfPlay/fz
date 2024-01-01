45632b7276fd483d48117318674220e6f383ea9e
JustOfPlay <126193393+JustOfPlay@users.noreply.github.com>
2024-01-01 18:24:54 +0100

    Update app.c

--- app.c
+++ app.c
@@ -1,11 +1,16 @@
-canvas_draw_box(canvas, 0, 0, 127, 63);
-canvas_set_color(canvas, ColorWhite);
+#include <furi.h>
+#include <gui/gui.h>
 
-canvas_draw_icon(canvas, 0, 0, &I_Background_128x11);
+int main() {
+    canvas_draw_box(canvas, 0, 0, 127, 63);
+    canvas_set_color(canvas, ColorWhite);
 
-canvas_draw_icon(canvas, 0, 52, &I_Background_128x11);
+    canvas_draw_icon(canvas, 0, 0, &I_Background_128x11);
 
-canvas_draw_icon(canvas, 117, 45, &I_Alert_9x8);
+    canvas_draw_icon(canvas, 0, 52, &I_Background_128x11);
 
-canvas_draw_icon(canvas, 118, 31, &I_Charging-lightning_mask_9x10);
+    canvas_draw_icon(canvas, 117, 45, &I_Alert_9x8);
 
+    canvas_draw_icon(canvas, 118, 31, &I_Charging - lightning_mask_9x10);
+
+}
