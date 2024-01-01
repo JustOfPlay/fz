654672d29c0771453f0e22af58520a899e317d26
JustOfPlay <126193393+JustOfPlay@users.noreply.github.com>
2024-01-01 21:24:02 +0100

    big update

--- app.c
+++ app.c
@@ -5,15 +5,48 @@
 const GpioPin* const pin_led = &gpio_ext_pa7;
 const GpioPin* const pin_back = &gpio_button_back;
 
-static void my_draw_callback(Canvas* canvas, void* context) {
+static void main(Canvas* canvas, void* context) {
     UNUSED(context);
+    canvas_draw_icon(canvas, 2, 1, &I_SDcardMounted_11x8);
+
+    canvas_draw_line(canvas, 0, 12, 125, 12);
+
+    canvas_draw_icon(canvas, 2, 22, &I_SDcardFail_11x8);
+
+    canvas_draw_icon(canvas, 16, 1, &I_Bluetooth_Idle_5x8);
+
+    canvas_draw_icon(canvas, 84, -1, &I_Voltage_16x16);
+
+    canvas_draw_icon(canvas, 23, 1, &I_Hidden_window_9x8);
+
+    canvas_set_font(canvas, FontSecondary);
+    canvas_draw_str(canvas, 52, 10, "12.30");
+
     canvas_set_font(canvas, FontPrimary);
-    canvas_draw_str(canvas, 5, 8, "GPIO - BLINK LED DEMO");
-    canvas_draw_str(canvas, 5, 22, "Connect long LED to pin");
-    canvas_draw_str(canvas, 5, 32, "A7.  Connect short LED");
-    canvas_draw_str(canvas, 5, 42, "to 220 ohm resistor.");
-    canvas_draw_str(canvas, 5, 52, "Connect other end of");
-    canvas_draw_str(canvas, 5, 62, "resistor to pin GND.");
+    canvas_draw_str(canvas, 102, 10, "100%");
+
+    canvas_draw_line(canvas, 100, 0, 126, 0);
+
+    canvas_draw_line(canvas, 127, 1, 127, 10);
+
+    canvas_draw_line(canvas, 126, 11, 100, 11);
+
+    canvas_draw_line(canvas, 99, 1, 99, 10);
+
+    canvas_draw_dot(canvas, 99, 3);
+
+    canvas_draw_line(canvas, 98, 2, 97, 2);
+
+    canvas_draw_line(canvas, 98, 9, 97, 9);
+
+    canvas_draw_line(canvas, 96, 3, 96, 8);
+
+    canvas_set_font(canvas, FontSecondary);
+    canvas_draw_str(canvas, 1, 67, "---*-----------------");
+
+    canvas_draw_icon(canvas, 50, 21, &I_Power_25x27);
+
+
 }
 
 int gpio_blink_app(void* p) {
