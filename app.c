c851769c92cb662dfab9506606589c682537a1f1
JustOfPlay <126193393+JustOfPlay@users.noreply.github.com>
2024-01-01 21:56:21 +0100

    fix

--- app.c
+++ app.c
@@ -17,17 +17,7 @@ const GpioPin* const pin_back = &gpio_button_back;
 
 static void delta_gui_app(Canvas* canvas, void* context) {
     UNUSED(context);
-    canvas_draw_icon(canvas, 2, 1, &I_SDcardMounted_11x8);
-
-    canvas_draw_line(canvas, 0, 12, 125, 12);
-
-    canvas_draw_icon(canvas, 2, 22, &I_SDcardFail_11x8);
-
-    canvas_draw_icon(canvas, 16, 1, &I_Bluetooth_Idle_5x8);
-
-    canvas_draw_icon(canvas, 84, -1, &I_Voltage_16x16);
-
-    canvas_draw_icon(canvas, 23, 1, &I_Hidden_window_9x8);
+    canvas_draw_line(canvas, 0, 13, 127, 13);
 
     canvas_set_font(canvas, FontSecondary);
     canvas_draw_str(canvas, 52, 10, "12.30");
@@ -54,7 +44,7 @@ static void delta_gui_app(Canvas* canvas, void* context) {
     canvas_set_font(canvas, FontSecondary);
     canvas_draw_str(canvas, 1, 67, "---*-----------------");
 
-    canvas_draw_icon(canvas, 50, 21, &I_Power_25x27);
+
 
 
 }
