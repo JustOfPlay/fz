5f111b9089f994fdbb944bce55dc525754a833d4
JustOfPlay <126193393+JustOfPlay@users.noreply.github.com>
2024-01-03 00:22:45 +0100

    Update app.c

--- app.c
+++ app.c
@@ -50,7 +50,7 @@ static void delta_gui_app(Canvas* canvas, void* context) {
 
     canvas_draw_icon(canvas, 15, 2, &I_Bluetooth_Connected_16x8);       //bluetooth on and connected
 
-    canvas_draw_icon(canvas, 25, 27, &'I_voldown_24x21');            //placeholder app
+    canvas_draw_icon(canvas, 25, 27, &I_voldown_24x21);            //placeholder app
 
     canvas_draw_icon(canvas, 77, 26, &I_volup_24x21);      //placeholder app
 
