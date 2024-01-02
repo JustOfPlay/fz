41cb55b617e9ea33102d65aa9999c3ad2a14b833
JustOfPlay <126193393+JustOfPlay@users.noreply.github.com>
2024-01-02 18:25:06 +0100

    fix

--- app.c
+++ app.c
@@ -62,14 +62,41 @@ static void delta_gui_app(Canvas* canvas, void* context) {
     //canvas_draw_icon(canvas, 50, 21, &I_Power_25x27);
 
 }
-int main() {
 
 
 
-    void* canvas;
-    void* context;
+int32_t main(void* p) {
+    UNUSED(p);
 
-    delta_gui_app(canvas, context);
+    InputEvent event;
 
+    FuriMessageQueue* event_queue = furi_message_queue_alloc(8, sizeof(InputEvent));
+
+    ViewPort* view_port = view_port_alloc();
+
+
+    view_port_draw_callback_set(view_port, draw_callback, NULL);
+
+
+    view_port_input_callback_set(view_port, input_callback, event_queue);
+
+
+    Gui* gui = furi_record_open(RECORD_GUI);
+    gui_add_view_port(gui, view_port, GuiLayerFullscreen);
+
+    while(true) {
+        furi_check(furi_message_queue_get(event_queue, &event, FuriWaitForever) == FuriStatusOk);
+
+        if(event.key == InputKeyBack) {
+            break;
+        }
+    }
+
+    furi_message_queue_free(event_queue);
+
+    gui_remove_view_port(gui, view_port);
+
+    view_port_free(view_port);
+    furi_record_close(RECORD_GUI);
     return 0;
 }
