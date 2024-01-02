54dd21ec1065d50fe3862be966d62d1fa63f9e02
JustOfPlay <126193393+JustOfPlay@users.noreply.github.com>
2024-01-02 15:45:35 +0100

    Update app.c

--- app.c
+++ app.c
@@ -24,11 +24,12 @@
 
 
 
+
 static void delta_gui_app() {// , void* context
     //UNUSED(context);
     //canvas_draw_icon(canvas, 2, 1, &I_SDcardMounted_11x8);
 
-    canvas_draw_line(Canvas, 0, 12, 125, 12);
+    canvas_draw_line(canvas, 0, 12, 125, 12);
 
     //canvas_draw_icon(canvas, 2, 22, &I_SDcardFail_11x8);
 
@@ -38,30 +39,30 @@ static void delta_gui_app() {// , void* context
 
     //canvas_draw_icon(canvas, 23, 1, &I_Hidden_window_9x8);
 
-    canvas_set_font(Canvas, FontSecondary);
-    canvas_draw_str(Canvas, 52, 10, "12.30");
+    canvas_set_font(canvas, FontSecondary);
+    canvas_draw_str(canvas, 52, 10, "12.30");
 
-    canvas_set_font(Canvas, FontPrimary);
-    canvas_draw_str(Canvas, 102, 10, "100%");
+    canvas_set_font(canvas, FontPrimary);
+    canvas_draw_str(canvas, 102, 10, "100%");
 
-    canvas_draw_line(Canvas, 100, 0, 126, 0);
+    canvas_draw_line(canvas, 100, 0, 126, 0);
 
-    canvas_draw_line(Canvas, 127, 1, 127, 10);
+    canvas_draw_line(canvas, 127, 1, 127, 10);
 
-    canvas_draw_line(Canvas, 126, 11, 100, 11);
+    canvas_draw_line(canvas, 126, 11, 100, 11);
 
-    canvas_draw_line(Canvas, 99, 1, 99, 10);
+    canvas_draw_line(canvas, 99, 1, 99, 10);
 
-    canvas_draw_dot(Canvas, 99, 3);
+    canvas_draw_dot(canvas, 99, 3);
 
-    canvas_draw_line(Canvas, 98, 2, 97, 2);
+    canvas_draw_line(canvas, 98, 2, 97, 2);
 
-    canvas_draw_line(Canvas, 98, 9, 97, 9);
+    canvas_draw_line(canvas, 98, 9, 97, 9);
 
-    canvas_draw_line(Canvas, 96, 3, 96, 8);
+    canvas_draw_line(canvas, 96, 3, 96, 8);
 
-    canvas_set_font(Canvas, FontSecondary);
-    canvas_draw_str(Canvas, 1, 67, "---*-----------------");
+    canvas_set_font(canvas, FontSecondary);
+    canvas_draw_str(canvas, 1, 67, "---*-----------------");
 
     //canvas_draw_icon(canvas, 50, 21, &I_Power_25x27);
 
