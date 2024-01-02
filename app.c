c2314f77c33af707c7f10e110d715df809391fc9
JustOfPlay <126193393+JustOfPlay@users.noreply.github.com>
2024-01-03 00:03:18 +0100

    Update app.c

--- app.c
+++ app.c
@@ -26,11 +26,38 @@ static void delta_gui_app(Canvas* canvas, void* context) {
     UNUSED(context);
 
     canvas_set_font(canvas, FontBigNumbers);
-    canvas_draw_str(canvas, 37, 14, "12:50");
+    canvas_draw_str(canvas, 37, 14, "12:50");       //Time
 
-    canvas_draw_line(canvas, 0, 15, 127, 15);
+    canvas_draw_line(canvas, 0, 15, 127, 15);       //Split line
 
-    canvas_draw_icon(canvas, 51, 50, &I_FaceCharging_29x14);
+    canvas_draw_icon(canvas, 2, 18, &I_SDcardFail_11x8);       //only visable if no sd card
+
+    canvas_draw_icon(canvas, 2, 2, &I_SDcardMounted_11x8);      //only visable if sd card
+
+    canvas_draw_icon(canvas, 15, 18, &I_Bluetooth_Idle_5x8);    //bluetooth on
+
+    canvas_draw_icon(canvas, 108, 2, &I_Charging-lightning_mask_9x10);      //charching
+
+    canvas_draw_box(canvas, 102, 5, 20, 4);            //edit 3. number for % value (max 0 | min 20)
+
+    canvas_draw_icon(canvas, 100, 3, &I_Battery_26x8);      //battery icon
+
+    canvas_draw_icon(canvas, -1, 27, &I_Power_25x27);       //placeholder app
+
+    canvas_draw_icon(canvas, 51, 20, &I_Power_hvr_25x27);       //placeholder app
+
+    canvas_draw_icon(canvas, 116, 54, &I_Pin_back_arrow_10x8);      //back arrow
+
+    canvas_draw_icon(canvas, 15, 2, &I_Bluetooth_Connected_16x8);       //bluetooth on and connected
+
+    canvas_draw_icon(canvas, 25, 27, &I_Vol_down_25x27);            //placeholder app
+
+    canvas_draw_icon(canvas, 77, 26, &I_Vol_up_25x27);      //placeholder app
+
+    canvas_draw_icon(canvas, 103, 26, &I_Mute_25x27);       //placeholder app
+
+    canvas_set_font(canvas, FontSecondary);
+    canvas_draw_str(canvas, 93, 62, "Hold (    )");         //text for back arrow
 
 
 
