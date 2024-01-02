5d4b45c091d26723c08b812d0d20646ac4484e39
JustOfPlay <126193393+JustOfPlay@users.noreply.github.com>
2024-01-02 23:18:30 +0100

    Update app.c

--- app.c
+++ app.c
@@ -16,53 +16,24 @@
 #include <string.h>
 #include <stdio.h>
 
-
-
 #include <assets_icons.h>
+
+
 
 #define BACKLIGHT_ON 1
 
-static void delta_gui_app(Canvas* canvas, void* context) {
+static void delta_gui_app(Canvas* canvas, void* context, InputEvent* event) {
     UNUSED(context);
 
-    //canvas_draw_icon(canvas, 2, 1, &I_SDcardMounted_11x8)
+    canvas_set_font(canvas, FontBigNumbers);
+    canvas_draw_str(canvas, 37, 14, "12:50");
 
-    canvas_draw_line(canvas, 0, 12, 125, 12);
+    canvas_draw_line(canvas, 0, 15, 127, 15);
 
-    //canvas_draw_icon(canvas, 2, 22, &I_SDcardFail_11x8);
-
-    canvas_draw_icon(canvas, 16, 1, &I_Bluetooth_Idle_5x8);
-
-    //canvas_draw_icon(canvas, 84, -1, &I_Voltage_16x16);
-
-    //canvas_draw_icon(canvas, 23, 1, &I_Hidden_window_9x8);
-
-    canvas_set_font(canvas, FontSecondary);
-    canvas_draw_str(canvas, 52, 10, "12.30");
-
-    canvas_set_font(canvas, FontPrimary);
-    canvas_draw_str(canvas, 102, 10, "100%");
-
-    canvas_draw_line(canvas, 100, 0, 126, 0);
-
-    canvas_draw_line(canvas, 127, 1, 127, 10);
-
-    canvas_draw_line(canvas, 126, 11, 100, 11);
-
-    canvas_draw_line(canvas, 99, 1, 99, 10);
-
-    canvas_draw_dot(canvas, 99, 3);
-
-    canvas_draw_line(canvas, 98, 2, 97, 2);
-
-    canvas_draw_line(canvas, 98, 9, 97, 9);
-
-    canvas_draw_line(canvas, 96, 3, 96, 8);
-
-    canvas_set_font(canvas, FontSecondary);
-    canvas_draw_str(canvas, 1, 67, "---*-----------------");
-
-    //canvas_draw_icon(canvas, 50, 21, &I_Power_25x27);
+    while (event->key == InputKeyLeft) {
+        canvas_set_font(canvas, FontPrimary);
+        canvas_draw_str(canvas, -1, 29, "Button Pressed (Left)");
+    }
 
 }
 static void input_callback(InputEvent* input_event, void* context) {
